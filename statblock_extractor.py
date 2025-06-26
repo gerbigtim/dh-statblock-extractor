731c909f6a532584cbdd335b4b7665684605dea3
Tim Gerbig <gerbig.tim@gmail.com>
2025-06-26 13:11:17 +0200

    Continue parsing; Add custom exception

--- statblock_extractor.py
+++ statblock_extractor.py
@@ -7,7 +7,7 @@ import re
 import json
 import spellchecker
 import string
-from typing import Dict
+from typing import Dict, List
 
 import spellchecker.spellchecker
 
@@ -158,18 +158,29 @@ def statblock_text_to_dict(statblock_text):
 
 def terminates_with_punctuation(text: str) -> bool:
   """Returns True if text terminates with punctuation character, else False"""
-  return bool(re.match(rf".*[{re.escape(string.punctuation)}]+$", text))
+  return bool(re.match(rf".*?[{re.escape(string.punctuation)}]+$", text))
 
 def remove_end_punctuation(text: str) -> str:
-  match = re.match(rf"(?P<text>.*?)(?P<punct>[{re.escape(string.punctuation)}]+)$", text)
+  match = re.match(rf"(?P<text>.+?)(?P<punct>[{re.escape(string.punctuation)}]+)$", text)
   if match:
     return match.group("text")
   else:
     return text
 
+def contains_numbers(text: str) -> bool:
+  return bool(re.search(r"\d", text))
+
+def no_word_chars(text: str) -> bool:
+  return bool(re.fullmatch(r"\W+", text))
+
+
 def are_combinable(word1: str, word2: str) -> bool:
   """Check if combining 2 words could possibly yield a new word."""
   if (word1 is None) or (word2 is None):
+    return False
+  if contains_numbers(word1) or contains_numbers(word2):
+    return False
+  if no_word_chars(word1) or no_word_chars(word2):
     return False
   if terminates_with_punctuation(word1):
     return False
@@ -188,9 +199,9 @@ def combined_more_likely(word1: str, word2: str, wf_dict: Dict) -> bool:
   return wf_dict[merged] > min(wf_dict[word1], wf_dict[word2])
 
 
-def repair_split_words(text: str) -> str:
-  spell = spellchecker.SpellChecker()
-  wf_dict = spell.word_frequency.dictionary
+def repair_split_words(text: str, wf_dict: Dict = None) -> str:
+  if not wf_dict:
+    wf_dict = spellchecker.SpellChecker().word_frequency.dictionary
   words = text.split()
   repaired = []
   word_idx = 0
@@ -217,20 +228,32 @@ def repair_split_words(text: str) -> str:
 
   return ' '.join(repaired)
 
+class StatblockParseError(Exception):
+  def __init__(self, message, line=None, state=None):
+    super().__init__(message)
+    self.line = line
+    self.state = state
+
+  def __str__(self):
+    return f"[{self.state}] Failed to parse line: \"{repr(self.line)}\" -> {super().__str__()}"
+
+
 class StatblockScraper:
   """A Scraper for Daggerheart Adversaries"""
   def __init__(self, pdf_path: str, page_range: tuple[int,int] = None):
     self.pdf_path = pdf_path
     self.doc = pymupdf.open(pdf_path)
     self.page_range = page_range
-    self.raw_pages = {}
+    self.page_text_dict = {}
     self.statblocks = []
+    self.spell = spellchecker.SpellChecker()
+    self.wf_dict = self.spell.word_frequency.dictionary
 
   def extract_text(self):
     start_idx, end_idx = self.page_range if self.page_range else (0, None)
     print(f"{self.page_range},{start_idx}, {end_idx}")
     for page_num, page in enumerate(self.doc[start_idx: end_idx], start=start_idx):
-      self.raw_pages[page_num] = page.get_text()
+      self.page_text_dict[page_num] = page.get_text()
 
   def clean_pages(self):
     def clean_unicode_pua_chars(text: str) -> str:
@@ -239,21 +262,149 @@ class StatblockScraper:
       text = text.translate(COMMON_CHAR_REPLACEMENTS | pua_replacements)
       return text
 
-    for page_num, page in self.raw_pages.items():
-      self.raw_pages[page_num] = clean_unicode_pua_chars(page)
+    def combine_split_words(text: str) -> List[str]:
+      # combine split words
+      lines = text.splitlines()
+      for line_idx, line in enumerate(lines):
+        lines[line_idx] = repair_split_words(line, wf_dict=self.wf_dict)
+      return "\n".join(lines)
 
-  def normalize_lines(self):
-    # split into lines, fix Unicode, strip lines
-    pass
+    for page_num, page_text in self.page_text_dict.items():
+      page_text = clean_unicode_pua_chars(page_text)
+      page_text = combine_split_words(page_text)
+      self.page_text_dict[page_num] = page_text
 
-  def detect_statblock_start(self, line_idx, lines):
-    pass
+  def detect_statblock_start(self):
+    detected_start = False
+    while not detected_start:
+      try:
+        detected_start = bool(re.match(r"Tier \d", self.lines[self.line_idx]))
+      except IndexError:
+        self.page_finished = True
+        return
+      self.line_idx += 1
+    self.line_idx -= 2 # move back to last statblock name line
+    # Check if name has 2 lines
+    if re.match(r"[A-Z ]:\s*$", self.lines[self.line_idx -1]):
+      self.line_idx -= 1 # move back one more line in case of 2 line name
 
-  def parse_statblock(self, line_idx, lines):
-    pass
+
+  def parse_statblock(self):
+    statblock_dict = {}
+
+    # get the name
+    state = "name"
+    statblock_dict["name"] = ""
+    # Check if name has 2 lines:
+    if re.match(r"[A-Z ]:\s*$", self.lines[self.line_idx]):
+      statblock_dict["name"] = self.lines[self.line_idx]
+      self.line_idx += 1
+    statblock_dict["name"] += self.lines[self.line_idx]
+    self.line_idx +=1
+
+    # parse Tier line
+    state = "tier"
+    match = re.match(r"Tier (?P<tier>\d) (?P<type>.*)$", self.lines[self.line_idx].strip())
+    if not match:
+      raise StatblockParseError("Could not parse Tier line", line=self.lines[self.line_idx], state=state)
+    statblock_dict["tier"] = match.group("tier")
+    statblock_dict["type"] = match.group("type")
+    self.line_idx += 1
+
+    # parse description
+    state = "description"
+    description_start_idx = self.line_idx
+    while not re.match("Motives & Tactics:", self.lines[self.line_idx]):
+      self.line_idx += 1
+    statblock_dict["description"] = " ".join(self.lines[description_start_idx:self.line_idx])
+
+    # parse motives & tactics
+    state = "motives"
+    motives_start_idx = self.line_idx
+    match = re.match(r"Motives & Tactics: (?P<motives>.*)", self.lines[self.line_idx].strip())
+    if not match:
+      raise StatblockParseError("Could not parse Motives line", line=self.lines[self.line_idx], state=state)
+    self.line_idx += 1
+    while not re.match("Difficulty:", self.lines[self.line_idx]):
+      self.line_idx += 1
+    statblock_dict["motives"] = " ".join([match.group("motives")] + self.lines[motives_start_idx+1:self.line_idx])
+
+    # parse difficulty line
+    state = "difficulty"
+    regex = r"Difficulty: (?P<ac>\d+) \| Thresholds: (?P<thresholds>\d+/\d+) \| HP: (?P<hp>\d+) \| Stress: (?P<stress>\d+)"
+    match = re.match(regex, self.lines[self.line_idx].strip())
+    if not match:
+      raise StatblockParseError("Could not parse Difficulty line", line=self.lines[self.line_idx], state=state)
+    statblock_dict["ac"] = match.group("ac")
+    statblock_dict["thresholds"] = match.group("thresholds")
+    statblock_dict["hp"] = match.group("hp")
+    statblock_dict["stress"] = match.group("stress")
+    self.line_idx += 1
+
+    # parse atk line
+    state = "atk"
+    regex = r"ATK: (?P<atk>[\+-]\d+) \| (?P<weapon>.+?): (?P<range>.+?) \| (?P<dmg>.+)$"
+    match = re.match(regex, self.lines[self.line_idx].strip())
+    if not match:
+      raise StatblockParseError("Could not parse ATK line", line=self.lines[self.line_idx], state=state)
+    statblock_dict["atk"] = match.group("atk")
+    statblock_dict["weapon"] = match.group("weapon")
+    statblock_dict["range"] = match.group("range")
+    statblock_dict["dmg"] = match.group("dmg")
+    self.line_idx += 1
+
+    # parse experiences
+    if not re.match("FEATURES", self.lines[self.line_idx]):
+      state = "experiences"
+      experiences_start_idx = self.line_idx
+      match = re.match(r"Experience: (?P<experiences>.*)", self.lines[self.line_idx].strip())
+      if not match:
+        raise StatblockParseError("Could not parse Experiences line", line=self.lines[self.line_idx], state=state)
+      self.line_idx += 1
+      while not re.match("FEATURES", self.lines[self.line_idx]):
+        self.line_idx += 1
+      statblock_dict["experiences"] = " ".join([match.group("experiences")] + self.lines[experiences_start_idx+1:self.line_idx])
+
+    # parse Features
+  #  state = "features"
+    #if not re.match("FEATURES", self.lines[self.line_idx].strip()):
+      #raise StatblockParseError("Expected Features line", line=self.lines[self.line_idx], state=state)
+    #self.line_idx += 1
+    #features_finished = False
+    #statblock_dict["features"] = []
+    #regex = r"(?P<name>\.+? - \w+): (?P<desc>.*)"
+    #while not features_finished:
+      #feature_start_idx = self.line_idx
+      #match = re.match(regex, self.lines[self.line_idx].strip())
+      #self.line_idx += 1
+      #if re.match(r"(\s*)|([A-Z]+)$", self.lines[self.line_idx]):
+        #features_finished = True
+        #continue
+    self.statblocks.append(statblock_dict)
+    return
+
+
+
+
+
 
   def save_to_json(self, path):
     pass
+
+  def run(self):
+    self.extract_text()
+    self.clean_pages()
+    for page_num, page_text in self.page_text_dict.items():
+      self.lines = page_text.splitlines()
+      self.line_idx = 0
+      self.page_finished = False
+
+      self.detect_statblock_start()
+      while not self.page_finished:
+        self.parse_statblock()
+        self.detect_statblock_start()
+
+
 
 
 
@@ -261,11 +412,10 @@ class StatblockScraper:
 
 
 def main():
-  Scraper = StatblockScraper("pdfs/Daggerheart_Core_Rulebook-5-20-2025.pdf", page_range=(210,240))
-  Scraper.extract_text()
-  Scraper.clean_pages()
+  Scraper = StatblockScraper("pdfs/Daggerheart_Core_Rulebook-5-20-2025.pdf", page_range=(238,239))
+  Scraper.run()
 
-  print(Scraper.raw_pages[1])
+  print(json.dumps(Scraper.statblocks[0], indent=4))
 
 
 
